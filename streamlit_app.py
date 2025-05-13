db289b61fd4f66909175e94226a9e8ac4e59dddf
mhmdashray72 <mhmdashray72@gmail.com>
2025-05-13 11:26:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,25 +25,14 @@ with st.sidebar:
     
     data = df.copy()
     source = st.sidebar.selectbox("Departure City", ['All'] + list(data['Source'].unique()))
-
-    
     destination = st.sidebar.selectbox("Arrival City", ['All'] + list(data['Destination'].unique()))
-    # data after Source and Destination selection
-
-
     duration = data[(data['Source'] == source) & (data['Destination'] == destination)]
-    
     airline = st.sidebar.selectbox("Airline Carrier", ['All'] + list(data['Airline'].unique()))
-
-
     add_info = st.sidebar.selectbox("Additional Services", ['All'] + list(data['Additional_Info'].unique()))
-
-
-
 
     st.sidebar.write("")
     st.sidebar.markdown("Made by [Hussein zayed](https://github.com/HusseinZayed)")
-
+    
 # filtering Function
 def filter(airline, source, destination, add_info):
     if airline=='All' and source=='All' and destination=='All' and add_info=='All':
