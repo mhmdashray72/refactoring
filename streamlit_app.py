91cc75f1bfa7fb6a62adc387c8837413c694be5a
mhmdashray72 <mhmdashray72@gmail.com>
2025-05-13 11:20:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,32 +25,26 @@ with st.sidebar:
     
     data = df.copy()
     source = st.sidebar.selectbox("Departure City", ['All'] + list(data['Source'].unique()))
-    # data after Source selection
-    if source != 'All':
-        data = data[data['Source'] == source]
+
     
     destination = st.sidebar.selectbox("Arrival City", ['All'] + list(data['Destination'].unique()))
     # data after Source and Destination selection
-    if destination != 'All':
-        data = data[data['Destination'] == destination]
+
 
     duration = data[(data['Source'] == source) & (data['Destination'] == destination)]
     
     airline = st.sidebar.selectbox("Airline Carrier", ['All'] + list(data['Airline'].unique()))
-    # data after Source and Destination and Month and Day selection and departure hour selection and airline selection
-    if airline != 'All':
-        data = data[data['Airline'] == airline]
+
 
     add_info = st.sidebar.selectbox("Additional Services", ['All'] + list(data['Additional_Info'].unique()))
 
-    filter_box = st.sidebar.selectbox("Filter Prices on", [None, 'Day', 'Month', 'Dep_Hour'])
 
 
 
     st.sidebar.write("")
     st.sidebar.markdown("Made by [Hussein zayed](https://github.com/HusseinZayed)")
 
-    # filtering Function
+# filtering Function
 def filter(airline, source, destination, add_info):
     if airline=='All' and source=='All' and destination=='All' and add_info=='All':
         filtered_data = data.copy()
